99abc82aaa34879973bf676c293660571f3241e5
Noah <azsnobird1227@yahoo.com>
2023-09-06 15:26:50 -0500

    test and event listener on change

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 
 
 async function main() {
-    let userID = localStorage.getItem("id");
+    let userID = (localStorage.getItem("id"));
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`);
     const convertPosts =await posts.json();
     const userTarget = document.querySelector(".post-list")
@@ -16,13 +16,25 @@ async function main() {
                     </p>
                 </div>`;
     }).join("");
-
-    console.log(await fetch('https://jsonplaceholder.typicode.com/posts?userId=0'))
-    console.log(convertPosts);
 }
 
-function onSearchChange() {
-    
+async function onSearchChange(event) {
+    const userID = event.target.value;
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`);
+    const convertPosts =await posts.json();
+    const userTarget = document.querySelector(".post-list")
+
+    userTarget.innerHTML = convertPosts.map((post) => {
+        return `<div class="post">
+                    <div class="post__title">
+                        ${post.title}
+                    </div>
+                    <p class="post__body">
+                        ${post.body}
+                    </p>
+                </div>`;
+    }).join("");
 }
+
 
 main();
